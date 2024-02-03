33dfec2b56607ae67bafa44912b47d28cc4daf30
jobkilonzo <jobkilonzo95@gmail.com>
2024-02-03 11:04:36 +0300

    Changes in layout file

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { Room } from "./Room";
 export default function Page() {
   return (
     <Room>
-      <h1 className="font-bpld"></h1>
+      <h1 className="font-xl">Get figma clone</h1>
     </Room>
   );
 }
